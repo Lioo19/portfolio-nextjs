f3b391d0e0d91e565eb266f9cc4d06064802d797
Linnea <linnea.j.m.olofsson@gmail.com>
2021-08-25 11:39:44 +0200

    Fixed dynamic routes, fixed all formatting

--- pages/about.js
+++ pages/about.js
@@ -3,17 +3,17 @@ import Link from 'next/link'
 import Layout from '../components/layout'
 
 export default function AboutMe() {
-    return (
-        <Layout>
-            <Head>
-                <title>About Me</title>
-            </Head>
-            <h1>About Me</h1>
-            <h2>
-                <Link href="/">
-                    <a>back home</a>
-                </Link>
-            </h2>
-        </Layout>
-    )
+  return (
+      <Layout>
+          <Head>
+              <title>About Me</title>
+          </Head>
+          <h1>About Me</h1>
+          <h2>
+              <Link href="/">
+                  <a>back home</a>
+              </Link>
+          </h2>
+      </Layout>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,16 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
-    return {
-        props: {
-            allPostsData
-        }
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
     }
+  }
 }
 
 export default function Home({ allPostsData }) {
@@ -28,13 +30,17 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, date, title }) => (
-                <li className={utilStyles.listItem} key={id}>
-                    {title}
-                    <br />
-                    {date}
-                </li>
-            ))}
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
         </ul>
       </section>
     </Layout>
