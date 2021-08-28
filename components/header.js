e6718c3d722b0db50d87790bcb25277700f5b96c
Linnea <linnea.j.m.olofsson@gmail.com>
2021-08-28 12:35:30 +0200

    Fixed icon

--- components/header.js
+++ components/header.js
@@ -6,9 +6,14 @@ import IconLinks from './iconlinks.js'
 export default function Header() {
   return (
       <>
+          <Head>
+              <script defer src="https://kit.fontawesome.com/7d1c7e7747.js"></script>
+          </Head>
           <nav className="flex items-center justify-between flex-wrap bg-teal p-6">
             <div className="flex items-center flex-no-shrink text-black mr-6">
-              <svg className="h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+                <a className="px-1 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75" href="https://www.facebook.com/Linnea.olofsson13">
+                  <i className="fas fa-angle-double-right text-lg leading-lg text-black opacity-75"></i>
+                </a>
               <span className="font-semibold text-xl tracking-tight">Linnéa Olofsson</span>
             </div>
             <div className="block lg:hidden">
