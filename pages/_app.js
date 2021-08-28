1e938054390b4975be518311db0b95986694c78f
Linnea <linnea.j.m.olofsson@gmail.com>
2021-08-28 12:28:51 +0200

    Added fontawesome and icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/global.css'
 import Header from '../components/header'
+import Head from 'next/head'
 import Footer from '../components/footer'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 
 export default function App({ Component,pageProps }) {
   return (
       <>
       <Header></Header>
+      <Head><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /></Head>
       <Component {...pageProps} />
       <Footer></Footer>
       </>
