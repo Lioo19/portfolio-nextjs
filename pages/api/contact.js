86fd3ce0d4ca85b14de80f8395babc04525e3b8a
Linnea <linnea.j.m.olofsson@gmail.com>
2021-09-02 14:20:10 +0200

    Contact form fully functional!

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,17 +1,42 @@
-import { username, password } from "./auth.js"
+import { burnerEmail, personalEmail } from "./auth.js"
+
 
 export default function (req, res) {
+    require('dotenv').config()
+    const password = process.env.password
 
     let nodemailer = require('nodemailer')
     const transporter = nodemailer.createTransport({
         port: 465,
         host: "smtp.gmail.com",
         auth: {
-            user: username,
+            user: burnerEmail,
             pass: password,
         },
         secure: true,
     })
 
-    console.log(req.body)
+    const mailData = {
+        from: burnerEmail,
+        to: personalEmail,
+        subject: `Message from ${req.body.name}, ${req.body.email}`,
+        text: req.body.message,
+        html: `<div>
+                  Message: ${req.body.message}
+              </div>
+              <div>
+                  Sent from: ${req.body.email}
+                  Sent by: ${req.body.name}
+              </div>`
+    }
+
+    transporter.sendMail(mailData, function (err, info) {
+        if(err)
+            console.log(err)
+        else {
+            console.log(info)
+        }
+    })
+
+    res.status(200)
 }
