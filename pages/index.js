b12353b7812e904def5488362be6397705c915e2
Linnea <linnea.j.m.olofsson@gmail.com>
2021-09-09 17:31:53 +0200

    small fixes for layout

--- pages/index.js
+++ pages/index.js
@@ -24,30 +24,30 @@ export default function Home({ allPortfolioData }) {
         <title>{siteTitle}</title>
       </Head>
       <div className=" w-full pt-10">
-        <section className="bg-yellow-100 text-center mx-10 mb-12 mt-1 rounded">
+        <section className="bg-yellow-100 text-center mx-10 mb-48 mt-1 rounded">
           <h1 className="pt-16 pb-1 text-4xl font-bold opacity-75">Linnéa Olofsson</h1>
           <h2 className="pb-10 pt-1 text-8xl font-bold text-baseyellow opacity-75">Developer</h2>
           <p className="pb-16 text-xl italic">~In profession and in life</p>
         </section>
         <section className="flex justify-around items-center pb-10 h-72 text-center">
-          <div className="h-full w-1/4 bg-baseyellow pt-16">
+          <div className="h-full w-1/5 rounded bg-baseyellow pt-16">
             <a href="/about">
               <p>About</p>
             </a>
           </div>
-          <div className="h-full w-1/4 bg-baseyellow pt-16">
+          <div className="h-full w-1/5 rounded bg-baseyellow pt-16">
             <a href="/portfolio">
               <p>Portfolio</p>
             </a>
           </div>
-          <div className="h-full w-1/4 bg-baseyellow pt-16">
+          <div className="h-full w-1/5 rounded bg-baseyellow pt-16">
             <a href="/contact">
               <p>Contact</p>
             </a>
           </div>
         </section>
         <section>
-          <div className="flex flex-row justify-center bg-baseyellow rounded py-10 my-10">
+          <div className="flex flex-row justify-center bg-baseyellow rounded py-10 my-24">
             <ContactForm></ContactForm>
           </div>
         </section>
