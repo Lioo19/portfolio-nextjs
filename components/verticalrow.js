8da60bddc7683cecccfced955965ce4de49fa0b8
Linnea <linnea.j.m.olofsson@gmail.com>
2021-09-10 16:32:59 +0200

    Created component for verticalRows on frontpage

---
+++ components/verticalrow.js
@@ -0,0 +1,30 @@
+import className from 'classnames';
+import { useRouter } from "next/router";
+
+//Props should include title, description, image, imageAlt, reverse(bool)
+export default function VerticalRow (props) {
+  const verticalSwitchClass = className(
+      "mt-20",
+      "flex",
+      "flex-wrap",
+      "items-center",
+      {
+        "flex-row-reverse": props.reverse,
+      }
+  );
+
+  const router = useRouter();
+
+  return (
+    <div className={verticalSwitchClass}>
+      <div className="w-full sm:w-1/2 text-center sm:px-6">
+        <h3 className="text-3xl text-gray-900 font-semibold">{props.title}</h3>
+        <div className="mt-6 text-xl leading-9">{props.description}</div>
+      </div>
+
+      <div className="w-full sm:w-1/2 p-6">
+        <img src={`${router.basePath}${props.image}`} alt={props.imageAlt} />
+      </div>
+    </div>
+  );
+};
--- components/verticalswitch.js
+++ components/verticalswitch.js
@@ -1,23 +1,21 @@
-
+import VerticalRow from './verticalrow';
 
 export default function VerticalSwitch () {
   return (
-      <section className="flex justify-around items-center pb-10 h-72 text-center">
-        <div className="h-full w-1/5 rounded bg-baseyellow pt-16">
-          <a href="/about">
-            <p>About</p>
-          </a>
-        </div>
-        <div className="h-full w-1/5 rounded bg-baseyellow pt-16">
-          <a href="/portfolio">
-            <p>Portfolio</p>
-          </a>
-        </div>
-        <div className="h-full w-1/5 rounded bg-baseyellow pt-16">
-          <a href="/contact">
-            <p>Contact</p>
-          </a>
-        </div>
-      </section>
+    <section className="px-1">
+      <VerticalRow
+          title="Title1"
+          description="description1 description1 description1 description1 description1 description1"
+          image="/images/tradgard2flipped.png"
+          imageAlt="FirstImageAlt1"
+      />
+      <VerticalRow
+          title="Title1"
+          description="description1"
+          image="/images/tradgard2flipped.png"
+          imageAlt="FirstImageAlt1"
+          reverse
+      />
+    </section>
   )
 }
